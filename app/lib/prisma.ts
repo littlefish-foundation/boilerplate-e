0f7d44dd1c2e333e4cad17ddb27b8dc6d4190805
Cem Karaca <cem.karaca@gmail.com>
2024-05-21 19:46:45 +0300

    Starting next-auth branch

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,6 +1,27 @@
 // @ts-nocheck
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
+const prismaClientSingleton = () => {
+  const neon = new Pool({
+    connectionString: process.env.POSTGRES_PRISMA_URL,
+  });
+  const adapter = new PrismaNeon(neon);
+  return new PrismaClient({ adapter });
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = global.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") global.prismaGlobal = prisma;
+
+/*
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
@@ -14,3 +35,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 export default prisma;
+
+*/
