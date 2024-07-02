52d3982ab27c4fe5e80306069f83d9dcfdfafc5a
Emir Olgun <emirolgun@gmail.com>
2024-07-02 20:47:05 +0300

    test files deleted

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -7,7 +7,6 @@ export default async function Page() {
     const users = await prisma.user.findMany();
     const session = await auth();
     const accessUser = session?.user;
-    console.log("f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a".length)
     if (accessUser?.verifiedPolicy != "admin") { 
         redirect("/");
     }
