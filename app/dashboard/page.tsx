968d2479cd4399b5729df460153ecb82ec04d36e
Cem Karaca <cem.karaca@gmail.com>
2024-07-06 02:54:32 +0300

    Working on monitoring page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -67,7 +67,7 @@ import {
 } from "@/components/ui/tooltip"
 
 import { SettingsPane } from "@/components/nft-auth/settings"
-
+import { MonitorPane } from "@/components/nft-auth/monitor"
 
 export default async function Dashboard() {
   return (
@@ -86,14 +86,7 @@ export default async function Dashboard() {
               
             </BreadcrumbList>
           </Breadcrumb>
-          <div className="relative ml-auto flex-1 md:grow-0">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search..."
-              className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
-            />
-          </div>
+          
           
         </header>
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
@@ -104,45 +97,11 @@ export default async function Dashboard() {
                 <TabsTrigger value="tokens">Tokens</TabsTrigger>
                 <TabsTrigger value="nfts">NFTs</TabsTrigger>
                 <TabsTrigger value="settings">Settings</TabsTrigger>
-                <TabsTrigger value="archived" className="hidden sm:flex">
+                <TabsTrigger value="monitor" className="hidden sm:flex">
                   Monitor (admin elevation)
                 </TabsTrigger>
               </TabsList>
-              <div className="ml-auto flex items-center gap-2">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="outline" size="sm" className="h-7 gap-1">
-                      <ListFilter className="h-3.5 w-3.5" />
-                      <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                        Filter
-                      </span>
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuCheckboxItem checked>
-                      Active
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>
-                      Archived
-                    </DropdownMenuCheckboxItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-                <Button size="sm" variant="outline" className="h-7 gap-1">
-                  <File className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Export
-                  </span>
-                </Button>
-                <Button size="sm" className="h-7 gap-1">
-                  <PlusCircle className="h-3.5 w-3.5" />
-                  <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                    Add Product
-                  </span>
-                </Button>
-              </div>
+              
             </div>
             <TabsContent value="dashboard">
               
@@ -468,24 +427,8 @@ export default async function Dashboard() {
             <TabsContent value="settings">
               <SettingsPane />
             </TabsContent>
-            <TabsContent value="nfts">
-              <Card x-chunk="dashboard-06-chunk-0">
-                <CardHeader>
-                  <CardTitle>NFTs</CardTitle>
-                  <CardDescription>
-                    Manage your products and view their sales performance.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  
-                </CardContent>
-                <CardFooter>
-                  <div className="text-xs text-muted-foreground">
-                    Showing <strong>1-10</strong> of <strong>32</strong>{" "}
-                    products
-                  </div>
-                </CardFooter>
-              </Card>
+            <TabsContent value="monitor">
+            <MonitorPane />
             </TabsContent>
           </Tabs>
         </main>
