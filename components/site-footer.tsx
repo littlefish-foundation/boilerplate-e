6b1baa45dd979cda126cc7792f7cb1e8495ce4d2
emir-olgun <emirolgun@gmail.com>
2025-01-28 18:33:17 +0300

    footer image fixed

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,5 +1,6 @@
 import { DiscordLogoIcon, GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
+import Image from "next/image";
 
 const footerNavs = [
   {
@@ -60,7 +61,13 @@ export function SiteFooter() {
         <div className="md:flex md:justify-between px-8 p-4 py-16 sm:pb-16 gap-4">
           <div className="mb-12 flex-col flex gap-4">
             <Link href="/" className="flex items-center gap-2">
-              <img src="findthefish.png" className="h-8 w-8 text-primary" />
+              <Image
+                src="/findthefish.png"
+                alt="littlefish logo"
+                width={32}
+                height={32}
+                className="text-primary"
+              />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 littlefish
               </span>
